f6350987d672e91243f7b9a8594ed0dd49189474
Ali Hassan <alehassaan@gmail.com>
2024-11-11 21:42:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+// Menu Open Close
+let menu = document.querySelector('.menu-icon');
+
+menu.onclick = () => {
+    menu.classList.toggle("move");
+};
